fd8cc00ac50e86651ca4ddcffeb25b441d4e0a0f
isaaczd-debug <isaaculloamucha@gmail.com>
2025-10-26 05:00:01 -0500

    Add files via upload

---
+++ arysu/src/test/java/com/example/arysu/ArysuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arysu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArysuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
